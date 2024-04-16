d701c00005635be362379eef2dd1391e9659a53c
Leo0liveira <leoufms@gmail.com>
2024-04-16 13:24:53 -0400

    Constructor

--- Classes/index.js
+++ Classes/index.js
@@ -1,9 +1,24 @@
 "use strict";
 /**
  * TODO: Campos em Classe
+ * Inicialização sem valores
  */
 class User {
 }
 const profile = new User();
 profile.name = "Leonardo";
 console.log(profile);
+/**
+ * TODO: Constructor
+ * Contructor é uma função que nos dá a possibilidade de iniciar um objeto com valores nos seus campos
+ * Isso faz com que não precisemos mais da notação "!"
+ * Os campos serão preenchidos na hora que instanciarmos um objeto
+ */
+class newUser {
+    constructor(name, age) {
+        this.name = name,
+            this.age = age;
+    }
+}
+const user1 = new newUser("Leo", 24);
+console.log(user1);
--- Classes/index.ts
+++ Classes/index.ts
@@ -1,5 +1,6 @@
 /**
  * TODO: Campos em Classe
+ * Inicialização sem valores
  */
 class User{
   name!: string
@@ -8,3 +9,22 @@ class User{
 const profile = new User()
 profile.name = "Leonardo"
 console.log(profile)
+
+/**
+ * TODO: Constructor
+ * Contructor é uma função que nos dá a possibilidade de iniciar um objeto com valores nos seus campos
+ * Isso faz com que não precisemos mais da notação "!"
+ * Os campos serão preenchidos na hora que instanciarmos um objeto
+ */
+class newUser{
+  name
+  age
+
+  constructor(name:string, age:number){
+    this.name = name,
+    this.age = age
+  }
+}
+const user1 = new newUser("Leo", 24)
+console.log(user1)
+
