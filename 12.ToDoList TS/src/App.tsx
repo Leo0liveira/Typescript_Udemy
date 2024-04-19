156b4aa16b5ba01e03cf7694d326bb475ed2341a
Leo0liveira <leoufms@gmail.com>
2024-04-19 09:03:12 -0400

    ToDoList React

---
+++ 12.ToDoList TS/src/App.tsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import styles from './App.module.css'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { ITask } from './interface/Task';
+
+function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (title: string): void => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.title !== title;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.getElementById("modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        title="Editar tarefa"
+        children={
+          <TaskForm
+          btnText="Editar"
+          taskList={taskList}
+          task={taskToUpdate}
+          handleUpdate={updateTask}
+          />
+        }
+      />
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.todo_form}>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm
+            taskList={taskList}
+            setTaskList={setTaskList}
+            btnText="Cadastrar"
+          />
+        </div>
+        <div className="todo-container">
+          <h2>Suas tarefas:</h2>
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 12.ToDoList TS/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS Todo</span> @ 2024
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
